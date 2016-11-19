cda8c15579640535ddac33cf07fe96f561c8c6e2
程勇桥 <chengyongqiao@sion.local>
2016-11-19 22:02:24 +0800

    state

---
+++ src/main/scala/ch6/Rng.scala
@@ -0,0 +1,103 @@
+package ch6
+
+/**
+  * Created by chengyongqiao on 19/11/2016.
+  */
+
+
+trait RNG {
+    def nextInt: (Int, RNG)
+    def positiveInt: (Int, RNG)
+}
+
+
+object RNG {
+
+    def simple(seed: Long): RNG = new RNG {
+        override def nextInt: (Int, RNG) = {
+            val seed2 = (seed*0x5DEECE66DL + 0xBL) &
+                ((1L << 48) - 1)
+            ((seed2 >>> 16).asInstanceOf[Int],
+                simple(seed2))
+        }
+
+        override def positiveInt: (Int, RNG) = {
+            val (rndInt, newRng) = nextInt
+            if (rndInt == Int.MinValue) (0, newRng) else (Math.abs(rndInt), newRng)
+        }
+
+    }
+
+
+    type Rand[+A] = RNG => (A, RNG)
+
+    val int: Rand[Int] = _.nextInt // (rng: RNG) => rng.nextInt
+
+    def unit[A](a: A): Rand[A] = rng => (a, rng)// (rng: RNG) => (a, rng)
+
+    def rngToIntAndRng(r: RNG):(Int, RNG) = r.nextInt
+
+    def map[A, B](s: Rand[A])(f: A => B): Rand[B] = {
+        (rng: RNG) => {
+            val (a, rng2) = s(rng)
+            (f(a), rng2)
+        }
+    }
+
+    def positiveMax(n: Int): Rand[Int] = {
+        (rng: RNG) => {
+            val s:Rand[Int] = (x:RNG) => {x.positiveInt}
+            val f = (x: Int) => if (x < n) x else x % n
+            RNG.map(s)(f)(rng)
+        }
+    }
+
+
+}
+
+
+
+
+object RngMain extends App {
+
+    val rng1 = RNG.simple(1020324)
+
+    println("same seed to generate random integer")
+
+    val (rndInt1, _) = rng1.nextInt
+    val (rndInt2, _) = rng1.nextInt
+    println(rndInt1, rndInt2, rndInt1 == rndInt2)
+
+    println("pass new seed through to generate random integer")
+
+    val (rndInt3, rng2) = rng1.nextInt
+    val (rndInt4, _) = rng2.nextInt
+    println(rndInt3, rndInt4, rndInt3 == rndInt4)
+
+
+    println("positive random integer")
+    val (rndInt5, _) = rng2.positiveInt
+    val (rndInt6, _) = rng2.positiveInt
+    val (rndInt7, _) = rng2.positiveInt
+    println(rndInt5, rndInt6, rndInt7)
+
+    val u1 = RNG.unit(1)
+    val (i1, rng2copy) = u1(rng2)
+    println(i1)
+    println(rng2 == rng2copy)
+
+    val (rndInt8, rng8) = RNG.rngToIntAndRng(rng2)
+    println(rndInt8, rng8)
+
+    println("map")
+    val rngDouble = RNG.map(RNG.int)(_ * 2.0)
+    println(rngDouble(rng8)._1)
+    println(rngDouble(rng8)._1)
+
+    println("positiveMax")
+    val max = RNG.positiveMax(100)
+    println(max(rng8)._1)
+    println(max(rng8)._1)
+    println(max(rng2)._1)
+    println(max(rng2)._1)
+}
